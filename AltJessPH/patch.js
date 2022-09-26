ab319a554bc5f042a53b927ca952aec2cda7cdca
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2022-09-26 18:23:32 +0100

    Add files via upload

---
+++ AltJessPH/patch.js
@@ -0,0 +1,9 @@
+{
+    "dependencies": [
+        "thpatch/lang_en",
+        "PKWeegee/ThAltJess"
+    ],
+    "id": "AltJessPH",
+
+    "title": "AltJessPH"
+}
---
+++ repo.js
@@ -0,0 +1,9 @@
+{
+    "contact": "TESM#3052",
+    "id": "TESM",
+    "patches": {
+        "AltJessPH": "AltJessPH"
+    },
+
+    "title": "TESM's Patches"
+}
