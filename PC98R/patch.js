d707d7d82961200076979fb4057118d85c81cedc
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-01-13 12:14:02 +0000

    Add files via upload

--- PC98R/patch.js
+++ PC98R/patch.js
@@ -1,10 +1,10 @@
 {
-	"dependencies": [
-		"thpatch/lang_en"
-	],
-	"id": "15Kaizo",
-	"servers": [
-		"https://mirrors.thpatch.net/TESM/15Kaizo/"
-	],
-	"title": "PC98 Reimu in touhou 18 stage 4"
+  "dependencies": [
+    "thpatch/lang_en"
+  ],
+  "id": "PC98R",
+  "servers": [
+    "https://mirrors.thpatch.net/TESM/PC98R/"
+  ],
+  "title": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage"
 }
