647580687205d0060cac4a4b244c4167d0a06e06
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-02-03 00:05:29 +0000

    Add files via upload

--- repo.js
+++ repo.js
@@ -9,7 +9,7 @@
         "15Kaizo": "15Kaizo",
         "15KaizoDLC": "Kaizo 15 DLC",
         "AltJessPH": "AltJessPH",
-        "Data10": "Touhou 10 data boss fights",
+        "Touhou 10 Kaizo": "Touhou 10 Kaizo",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage"
     }
 }
