7c023183093887652389f2d1679db35aa2786f51
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-02-06 00:16:19 +0000

    Add files via upload

---
+++ Yuuka/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en",
+	],
+	"id": "YuukaBoss16",
+	"servers": [
+		"https://mirrors.thpatch.net/TESM/Yuuka/"
+	],
+	"title": "Yuuka in touhou 16 AS A BOSS!?!? extra stage"
+}
--- repo.js
+++ repo.js
@@ -10,6 +10,7 @@
         "15KaizoDLC": "Kaizo 15 DLC",
         "AltJessPH": "AltJessPH",
         "Touhou 10 Kaizo": "Touhou 10 Kaizo",
-        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage"
+        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
+		"Yuuka": "Yuuka in touhou 16 AS A BOSS!?!? extra stage"
     }
 }
