b415f9aa80ae35fa8166e64410a4dd1c52f53441
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-02-16 23:55:14 +0000

    Add files via upload

---
+++ WilhelmvonClausewitzHalcyonHISUIMARU/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"id": "WilhelmvonClausewitzHalcyonHISUIMARU",
+	"servers": [
+		"https://mirrors.thpatch.net/TESM/Data10/"
+	],
+	"title": "WilhelmvonClausewitzHalcyonHISUIMARU in touhou 11 as the TRUE FINAL boss"
+}
--- repo.js
+++ repo.js
@@ -10,6 +10,7 @@
         "15KaizoDLC": "Kaizo 15 DLC",
         "AltJessPH": "AltJessPH",
         "Kaizo10": "touhou 10 Kaizo",
-        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage"
+        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
+		"WilhelmvonClausewitzHalcyonHISUIMARU": "WilhelmvonClausewitzHalcyonHISUIMARU"
     }
 }
