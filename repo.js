a683a37376e1fe3f8e3361b1d53f173494167d31
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-03-07 02:31:35 +0000

    Add files via upload

--- repo.js
+++ repo.js
@@ -11,6 +11,6 @@
         "AltJessPH": "AltJessPH",
         "Kaizo10": "touhou 10 Kaizo",
 		"13Kaizo": "touhou 13 Kaizo",
-        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
+        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage"
     }
 }
