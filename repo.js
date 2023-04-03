7324a8b0994d2f28645b54843452ca75ec09005f
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-04-03 01:40:03 +0100

    Add files via upload

--- repo.js
+++ repo.js
@@ -6,10 +6,11 @@
         "https://mirrors.thpatch.net/TESM/"
     ],
     "patches": {
+        "13Kaizo": "touhou 13 Kaizo",
         "15Kaizo": "15Kaizo",
         "15KaizoDLC": "Kaizo 15 DLC",
         "AltJessPH": "AltJessPH",
-        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "Touhou 10 Kaizo": "Touhou 10 Kaizo",
+        "Kaizo10": "touhou 10 Kaizo",
+        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage"
     }
 }
