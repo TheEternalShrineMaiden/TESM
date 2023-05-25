4ce6c28c33355e8c03bf504d57e76a4f3f4d82ff
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-05-25 21:57:08 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -14,6 +14,5 @@
         "Data10": "Touhou 10 Kaizo",
         "KaizoIN": "Kaizo IN",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "blueziffern": "NSFW Replaces Boss Protraits with ziffern's art work NSFW"
     }
 }
