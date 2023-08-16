52a289615f2ed1c4302dd54b67d816f8a8abb7c1
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-08-16 15:49:36 +0100

    Add files via upload

--- TESMgoofs/patch.js
+++ TESMgoofs/patch.js
@@ -1,6 +1,10 @@
 {
   "dependencies": [
-    "thpatch/lang_en"
+    "thpatch/lang_en",
+	"nmlgc/base_tsa",
+    "ExpHP/base_exphp",
+    "ExpHP/anm_leak",
+    "ExpHP/sprite_death_fix"
   ],
   "id": "TESMgoofs",
   "servers": [
