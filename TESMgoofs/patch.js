1edd03b607a9da2221cda4cb8346f12adaba512f
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-08-22 20:16:07 +0100

    Add files via upload

--- TESMgoofs/patch.js
+++ TESMgoofs/patch.js
@@ -1,10 +1,10 @@
 {
-	"dependencies": [
-		"thpatch/lang_en"
-	],
-	"id": "TESMgoofs",
-	"servers": [
-		"https://mirrors.thpatch.net/TESM/TESMgoofs/"
-	],
-	"title": "TESMgoofs"
+  "dependencies": [
+  "thpatch/lang_en"
+  ],
+  "id": "TESMgoofs",
+  "servers": [
+    "https://mirrors.thpatch.net/TESM/TESMgoofs/"
+  ],
+  "title": "TESMgoofs"
 }
