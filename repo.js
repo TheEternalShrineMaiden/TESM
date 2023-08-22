bfce75df4f05b2e9eeb4b65b9ff55c8d2e59cc37
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-08-22 13:42:40 +0100

    Add files via upload

--- repo.js
+++ repo.js
@@ -10,11 +10,11 @@
         "15Kaizo": "15Kaizo",
         "15KaizoDLC": "Kaizo 15 DLC",
         "16Kaizo": "16Kaizo",
-	"TESMgoofs": "Touhou but make some goofy shit(th06wip)",
         "AltJessPH": "AltJessPH",
         "Data10": "Touhou 10 Kaizo",
         "KaizoIN": "Kaizo IN",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
+        "TESMgoofs": "Touhou but make some goofy shit(th06wip)",
         "blueziffern": "NSFW Replaces Boss Protraits with ziffern's art work NSFW"
     }
 }
