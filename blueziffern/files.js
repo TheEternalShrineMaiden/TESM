7342b93e944707925346736ade3fade965b16ebc
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-08-23 11:56:12 +0100

    Add files via upload

--- blueziffern/files.js
+++ blueziffern/files.js
@@ -1,1 +1,18 @@
-{}
+{
+  "patch.js": 416947844,
+  "th06/blueziffern link.txt": 1983675616,
+  "th06/face03a.png": 1779093476,
+  "th06/face03b.png": 3063155272,
+  "th06/face05a.png": 2516887346,
+  "th06/face06a.png": 1810312056,
+  "th06/face06b.png": 3567989497,
+  "th06/face08a.png": 122567875,
+  "th06/face08b.png": 1174381558,
+  "th06/face09a.png": 1913782964,
+  "th06/face09b.png": 4135870858,
+  "th06/face10a.png": 302885381,
+  "th06/face10b.png": 408529445,
+  "th06/face12a.png": 2134187130,
+  "th06/face12b.png": 1688103495,
+  "th06/face12c.png": 2674206922
+}
