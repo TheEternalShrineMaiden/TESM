84f03539b771220495bf5501cd6861683f75cc20
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-08-25 23:37:11 +0100

    Add files via upload

--- TESMgoofs/patch.js
+++ TESMgoofs/patch.js
@@ -1,14 +1,10 @@
 {
   "dependencies": [
-    "thpatch/lang_en",
-    "ExpHP/base_exphp",
-    "ExpHP/anm_leak",
-    "ExpHP/sprite_death_fix",
-    "ExpHP/bullet_cap"
+    "thpatch/lang_en"
   ],
   "id": "TESMgoofs",
   "servers": [
     "https://mirrors.thpatch.net/TESM/TESMgoofs/"
   ],
-  "title": "Touhou but make some goofy shit(th06wip)"
+  "title": "TESMgoofs"
 }
