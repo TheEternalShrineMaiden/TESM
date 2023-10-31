4a8722c32fbe3e9cbc5d3428d534b17eba0206ed
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-10-31 08:18:32 +0000

    Add files via upload

--- TESMgoofs/patch.js
+++ TESMgoofs/patch.js
@@ -1,14 +1,15 @@
 {
-  "dependencies": [
-    "ExpHP/base_exphp",
-    "ExpHP/anm_leak",
-    "ExpHP/sprite_death_fix",
-    "ExpHP/bullet_cap",
-    "thpatch/lang_en"
-  ],
-  "id": "TESMgoofs",
-  "servers": [
-    "https://mirrors.thpatch.net/TESM/TESMgoofs/"
-  ],
-  "title": "TESMgoofs in th15 and th06. and now a pure WIP on touhou 19 atm"
+	"dependencies": [
+		"SuperChrim/Th19StoryBGMChange",
+		"ExpHP/base_exphp",
+		"ExpHP/anm_leak",
+		"ExpHP/sprite_death_fix",
+		"ExpHP/bullet_cap",
+		"thpatch/lang_en"
+	],
+	"id": "TESMgoofs",
+	"servers": [
+		"https://mirrors.thpatch.net/TESM/TESMgoofs/"
+	],
+	"title": "TESMgoofs in th15 and th06. and now a pure WIP on touhou 19 atm"
 }
