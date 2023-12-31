b8110f4f20213b25172a2831a4eb3f8ec00494a4
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2023-12-31 02:45:43 +0000

    Update repo.js

--- repo.js
+++ repo.js
@@ -14,7 +14,6 @@
         "KaizoIN": "Kaizo IN",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
         "TESMgoofs": "TESMgoofs in th15 and th06. and now a pure WIP on touhou 19 atm",
-	"THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of",
         "blueziffern": "blueziffern"
     }
 }
