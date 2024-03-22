006be5a1d293660d8268e8ad4ccae6d1fb006529
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2024-03-22 07:11:08 +0000

    Add files via upload

---
+++ TESM/repo.js
@@ -0,0 +1,18 @@
+{
+    "id": "TESM",
+    "title": "TESM#3052",
+    "servers": [
+        "https://mirrors.thpatch.net/TESM/"
+    ],
+    "patches": {
+        "13Kaizo": "Touhou 13 Kaizo",
+        "15Kaizo": "15Kaizo",
+        "16Kaizo": "16Kaizo",
+        "AltJessPH": "AltJessPH",
+        "Data10": "Touhou 10 Kaizo",
+        "KaizoIN": "Kaizo IN",
+        "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
+        "TESMgoofs": "TESMgoofs in th15 and th06. and now a pure WIP on touhou 19 atm",
+        "blueziffern": "blueziffern"
+    }
+}
