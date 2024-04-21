165e2fb2146cbd0c57b51443738e0d5da6a1f45c
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2024-04-21 02:16:27 +0100

    Add files via upload

---
+++ THMiDI/files.js
@@ -0,0 +1,4 @@
+{
+  "patch.js": 3712457009,
+  "th16/title/title_logo.png": 1791873159
+}
---
+++ THMiDI/patch.js
@@ -0,0 +1,15 @@
+{
+  "dependencies": [
+    "SuperChrim/Th19StoryBGMChange",
+    "ExpHP/base_exphp",
+    "ExpHP/anm_leak",
+    "ExpHP/sprite_death_fix",
+    "ExpHP/bullet_cap",
+    "thpatch/lang_en"
+  ],
+  "id": "THMiDI",
+  "servers": [
+    "https://mirrors.thpatch.net/TESM/THMiDI/"
+  ],
+  "title": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of"
+}
