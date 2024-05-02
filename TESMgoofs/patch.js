bd5287092b92e40869bbb29c063878f052675164
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2024-05-02 09:26:26 +0100

    Add files via upload

--- TESMgoofs/patch.js
+++ TESMgoofs/patch.js
@@ -3,6 +3,7 @@
     "SuperChrim/Th19StoryBGMChange",
     "ExpHP/base_exphp",
     "ExpHP/anm_leak",
+    "ExpHP/continue",
     "ExpHP/sprite_death_fix",
     "ExpHP/bullet_cap",
     "thpatch/lang_en"
