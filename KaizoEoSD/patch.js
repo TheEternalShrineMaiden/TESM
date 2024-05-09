4a1ffa53a066d8f39343cfd7a1cea1c300c1bd28
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2024-05-09 08:37:34 +0100

    Add files via upload

---
+++ KaizoEoSD/patch.js
@@ -0,0 +1,15 @@
+{
+	"dependencies": [
+		"SuperChrim/Th19StoryBGMChange",
+		"ExpHP/base_exphp",
+		"ExpHP/anm_leak",
+		"ExpHP/sprite_death_fix",
+		"ExpHP/bullet_cap",
+		"thpatch/lang_en"
+	],
+	"id": "KaizoEoSD",
+	"servers": [
+		"https://mirrors.thpatch.net/TESM/KaizoEoSD/"
+	],
+	"title": "KaizoEoSD"
+}
