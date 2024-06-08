e5b3c47048d1bfad1e6527143b8c9a95df14de65
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2024-06-08 09:32:02 +0100

    Add files via upload

---
+++ LoDDCK/patch.js
@@ -0,0 +1,15 @@
+{
+  "dependencies": [
+		"SuperChrim/Th19StoryBGMChange",
+		"ExpHP/base_exphp",
+		"ExpHP/anm_leak",
+		"ExpHP/sprite_death_fix",
+		"ExpHP/bullet_cap",
+		"thpatch/lang_en"
+  ],
+  "id": "LoDDCK",
+  "servers": [
+    "https://mirrors.thpatch.net/TESM/LoDDCK/"
+  ],
+  "title": "LoDDCK"
+}
--- repo.js
+++ repo.js
@@ -12,9 +12,10 @@
         "AltJessPH": "AltJessPH",
         "Data10": "Touhou 10 Kaizo",
         "KaizoIN": "Kaizo IN",
+	    "TESMgoofs": "TESMgoofs in th15 and th06, th07",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-	    "KaizoEoSD": "Makes EoSD Harder and more intresting",
-        "TESMgoofs": "TESMgoofs in th15 and th06, th07",
-        "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of",
+        "KaizoEoSD": "Makes EoSD Harder and more intresting",
+        "14+15Kaizo": "Combines LoDDK + Someguy's Touhou 14 Kaizo patch + My touhou 15 Kaizo patch (only bosses lmao)",
+        "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of"
     }
 }
