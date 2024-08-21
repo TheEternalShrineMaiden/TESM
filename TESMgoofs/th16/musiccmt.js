c54644c1ff4b08c6a6dc941092de2ca7f2712690
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2024-08-21 17:51:10 +0100

    Add files via upload

---
+++ TESMgoofs/th16/musiccmt.js
@@ -0,0 +1,167 @@
+{
+    "1": [
+        "@",
+        "",
+        "This is the title screen theme.",
+        "",
+        "A theme with a \"we're about to begin!\" sort of feeling.",
+        "It ended up having a Japanese-style, light-hearted, and somewhat serious feel to it."
+    ],
+    "2": [
+        "@",
+        "",
+        "This is the Stage 1 theme.",
+        "",
+        "I tried to bring out an \"introductory\" feeling, and the exhilaration of summer.",
+        "It's a pretty light theme even by Touhou standards. Did I manage to express a",
+        "feeling of speed, I wonder?",
+        "With all said and done, it goes well with beer."
+    ],
+    "3": [
+        "@",
+        "",
+        "This is Eternity Larva's theme.",
+        "",
+        "I went for a feeling of a fairy with something mysterious hidden within her.",
+        "I think butterflies that appear in video games are definitely a symbol",
+        "of mysteriousness.",
+        "I mean, she's just a fairy so her head's empty, but still."
+    ],
+    "4": [
+        "@",
+        "",
+        "This is the stage 2 theme.",
+        "",
+        "In contrast to Stage 1's unending cheer, I added a sort of melancholy to this one",
+        "and brought out an autumnal feel.",
+        "Tailoring the music to sound uniform across the whole game, even as the seasons",
+        "change, is kind of fun."
+    ],
+    "5": [
+        "@",
+        "",
+        "This is Nemuno Sakata's theme.",
+        "",
+        "I think this theme turned out particularly video-gamey.",
+        "Yikes, it's a yamanba. They show up often in folk tales, but in spite of that",
+        "they're a pretty hard creature to pin down.",
+        "Using eerieness and hot-blooded fervor as my base, I made a Touhou-style theme."
+    ],
+    "6": [
+        "@",
+        "",
+        "This is the stage 3 theme.",
+        "",
+        "The concept for this one is flying high in the night sky,",
+        "where only the cherry blossoms are shining in the moonlight.",
+        "",
+        "Cherry blossoms always have a sense of fleetingness about them, and this time is no different."
+    ],
+    "7": [
+        "@",
+        "",
+        "This is Aunn Komano's theme.",
+        "",
+        "A feeling of spring, bravery, and a somewhat-strong opponent, as well as that of a mascot.",
+        "It's an all-over-the-place theme with all those things crammed into it.",
+        "Her character concept is \"a single divine beast acting as a pair,\" but she doesn't have",
+        "a split personality or anything. Also, you can't see it, but she's got herself a tail!"
+    ],
+    "8": [
+        "@",
+        "",
+        "This is the Stage 4 theme.",
+        "",
+        "\"Winter\" means \"blizzards,\" that's for sure.",
+        "You might not know this if you don't live in a snowy country, but blizzards are actually different",
+        "from rainstorms. They happen when snow gets whipped up off the ground and blows back down on a",
+        "horizontal slant. Wearing a straw hat in one is pointless."
+    ],
+    "9": [
+        "@",
+        "",
+        "This is Narumi Yatadera's theme.",
+        "",
+        "I composed it with an image of someone with her feet planted firmly on the ground. The original ",
+        "Jizo Bodhisattva is an honorable figure who provides salvation in the long era until Maitreya ",
+        "appears, but at some point or another, a countless number of eccentric Jizo statues spread ",
+        "throughout all of Japan. So I figure it'd be nice if there was a magic Jizo somewhere in there."
+    ],
+    "10": [
+        "@",
+        "",
+        "This is the Stage 5 theme.",
+        "",
+        "A world that's creepier than anything in Touhou thus far.",
+        "Although, when you consider the fact that all those doors are connected ",
+        "to people's backs, I guess you could call it convenient in a way.",
+        "They seem like they'd be kind of lukewarm, those doors."
+    ],
+    "11": [
+        "@",
+        "",
+        "This is Mai Teireida and Satono Nishida's theme.",
+        "",
+        "I incorporated a great deal of things into this song: eeriness, lightheartedness,",
+        "strong hostility, innocence, and on top of all that, a midboss-esque feeling.",
+        "I put enough in for both characters, so it's an extravagant song in a number of ways. ",
+        "Created with an image of them dancing madly and eerily."
+    ],
+    "12": [
+        "@",
+        "",
+        "This is the Stage 6 theme.",
+        "",
+        "Made with an image of a high-speed stage before the boss.",
+        "I think it exudes a feeling of being rushed along, ",
+        "regardless of the nature of the story going on."
+    ],
+    "13": [
+        "@",
+        "",
+        "This is Okina Matara's theme.",
+        "",
+        "A very \"This is the climax!\" sort of song with a lot of strength to it.",
+        "It has a strong melody, even compared to the other songs in the game, so it might've",
+        "been nice if I just went all-in and started singing lyrics to it. It's very likely that ",
+        "you'd start laughing too hard to even play the game, but that might actually be the best outcome."
+    ],
+    "14": [
+        "@",
+        "",
+        "This is the Extra Stage theme.",
+        "",
+        "A mysterious world that we dive into once more, having mustered up our resolve.",
+        "It's a song that combines both eeriness and gentleness.",
+        "",
+        "The song does a good job of conveying the stage's difficulty, so it's pretty depressing. (lol)"
+    ],
+    "15": [
+        "@",
+        "",
+        "The theme for Okina Matara (True).",
+        "",
+        "Even though it's an Extra Stage, it wound up being part of the main story.",
+        "Since it's your true battle with Okina, I swept away all the refreshing feeling of nature,",
+        "but still made it a song with a strong melody befitting of the true final boss.",
+        "Honestly, getting attacked from behind is just the worst!"
+    ],
+    "16": [
+        "@",
+        "",
+        "This is the ending theme.",
+        "",
+        "It kind of feels like a bad ending, but to tell the truth,",
+        "all the endings this time are like bad endings."
+    ],
+    "17": [
+        "@",
+        "",
+        "This is the staff roll theme.",
+        "",
+        "It's a different version of the winter stage theme made with an image of a refreshing conclusion.",
+        "",
+        "Speaking of which. Why is a \"winter blizzard\" song refreshing, you ask?",
+        "That's because I made it with \"early morning\" in mind. <i$Fuyu wa tsutomete>, as they say."
+    ]
+}
