7adc5ec0110ba3809325d7a8af2733e31a7f5108
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2024-12-29 23:10:10 +0000

    Add files via upload

---
+++ Impossible_Character/patch.js
@@ -0,0 +1,17 @@
+{
+  "dependencies": [
+    "SuperChrim/Th19StoryBGMChange",
+    "ExpHP/base_exphp",
+    "ExpHP/anm_leak",
+    "ExpHP/continue",
+    "ExpHP/sprite_death_fix",
+    "ExpHP/bullet_cap",
+    "zero318/ZeroEclExtensions",
+    "thpatch/lang_en"
+  ],
+  "id": "Impossible_Character",
+  "servers": [
+    "https://mirrors.thpatch.net/TESM/Impossible_Character/"
+  ],
+  "title": "Touhou 14 with a few ISC patterns"
+}
