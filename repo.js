1394fd45c48d8b7ed1cd829f39d25d34a34900ce
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-02-02 19:36:00 +0000

    Add files via upload

--- repo.js
+++ repo.js
@@ -7,14 +7,16 @@
     ],
     "patches": {
         "13Kaizo": "Touhou 13 Kaizo",
+        "14thHRTP": "uses the ying yanf from 13D's and makes a HTRP TESM things",
         "15Kaizo": "15Kaizo",
         "16Kaizo": "16Kaizo",
         "AltJessPH": "AltJessPH",
         "Data10": "Touhou 10 Kaizo",
+        "Impossible_Character": "Touhou 14 with a few ISC patterns",
         "KaizoEoSD": "KaizoEoSD",
         "KaizoIN": "Kaizo IN",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "TESMgoofs": "TESMgoofs in th15, th06, th07 and th08. and now a pure WIP on touhou 19 atm",
+        "TESMgoofs": "TESMgoofs in th15, th15, th06, th07. will be working on other stuff not sure when it'll be out so keep tight",
         "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of"
     }
 }
