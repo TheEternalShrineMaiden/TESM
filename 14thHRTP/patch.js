8a6931e41f9b5791b45f177bb8edd0c37da73f3d
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-02-02 19:39:27 +0000

    Add files via upload

---
+++ 14thHRTP/patch.js
@@ -0,0 +1,17 @@
+{
+	"dependencies": [
+		"SuperChrim/Th19StoryBGMChange",
+		"ExpHP/base_exphp",
+		"ExpHP/anm_leak",
+		"ExpHP/continue",
+		"ExpHP/sprite_death_fix",
+		"ExpHP/bullet_cap",
+		"zero318/ZeroEclExtensions",
+		"thpatch/lang_en"
+	],
+	"id": "14thHRTP",
+	"servers": [
+		"https://mirrors.thpatch.net/TESM/14thHRTP/"
+	],
+	"title": "uses the ying yanf from 13D's and makes a HTRP TESM things"
+}
