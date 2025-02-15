87ca5a6806acfa369b6c075d91f13da21a22b6f8
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-02-15 22:52:34 +0000

    Add files via upload

---
+++ dddddddddddd/th14/spells.js
@@ -0,0 +1,7 @@
+{
+    "64": "Overflow 「Samidare Shitstorm」",
+    "68": "Transgression 「Rust Emanating from the Body」",
+    "72": "Devil Storm 「Homun-cross」",
+    "76": "Mystery 「Card of Arcana」",
+    "80": "Creation Card 「Philosopher's Will」"
+}
