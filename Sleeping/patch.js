6b48378014fff21ee8fffef138aafb9cc876a19d
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-02-16 05:02:07 +0000

    Add files via upload

---
+++ Sleeping/patch.js
@@ -0,0 +1,17 @@
+{
+	"dependencies": [
+		"SuperChrim/Th19StoryBGMChange",
+		"ExpHP/base_exphp",
+		"ExpHP/anm_leak",
+		"ExpHP/continue",
+		"ExpHP/sprite_death_fix",
+		"ExpHP/bullet_cap",
+		"zero318/ZeroEclExtensions",
+		"thpatch/lang_en"
+	],
+	"id": "SBB",
+	"servers": [
+		"https://mirrors.thpatch.net/TESM/Sleeping/"
+	],
+	"title": "Sleeping Broken Border. orginal incident idea"
+}
