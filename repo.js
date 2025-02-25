183b4096e26f2b8f9a0e7aee31b0ef0136e7147b
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-02-25 15:26:02 +0000

    Add files via upload

--- repo.js
+++ repo.js
@@ -6,6 +6,7 @@
         "https://mirrors.thpatch.net/TESM/"
     ],
     "patches": {
+        "SoloSakuya": "putting Solo Sakuya in other games for a laugh",
         "13Kaizo": "Touhou 13 Kaizo",
         "15Kaizo": "15Kaizo",
         "16Kaizo": "16Kaizo",
@@ -13,8 +14,8 @@
         "Data10": "Touhou 10 Kaizo",
         "KaizoEoSD": "KaizoEoSD",
         "KaizoIN": "Kaizo IN",
+        "LoDDCK": "LoDDCK",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "Sleeping": "Sleeping Broken Border. orginal incident idea for PCB maybe?",
         "TESMgoofs": "TESMgoofs in th15, th06, th07 and th08. and now a pure WIP on touhou 19 atm",
         "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of"
     }
