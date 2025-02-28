a8e4fc5617c4dbefa8be560cf4fb30097acdae7f
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-02-28 01:52:59 +0000

    Add files via upload

---
+++ SoloReimu/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"id": "SoloReimu",
+	"servers": [
+		"https://mirrors.thpatch.net/TESM/SoloReimu/"
+	],
+	"title": "putting Solo Reimu in other games for a laugh"
+}
--- repo.js
+++ repo.js
@@ -6,17 +6,19 @@
         "https://mirrors.thpatch.net/TESM/"
     ],
     "patches": {
-        "SoloSakuya": "putting Solo Sakuya in other games for a laugh",
         "13Kaizo": "Touhou 13 Kaizo",
         "15Kaizo": "15Kaizo",
         "16Kaizo": "16Kaizo",
         "AltJessPH": "AltJessPH",
         "Data10": "Touhou 10 Kaizo",
+        "Impossible_Character": "Touhou 14 with a few ISC patterns",
         "KaizoEoSD": "KaizoEoSD",
         "KaizoIN": "Kaizo IN",
-        "LoDDCK": "LoDDCK",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "TESMgoofs": "TESMgoofs in th15, th06, th07 and th08. and now a pure WIP on touhou 19 atm",
+        "Sleeping": "Sleeping Broken Border. orginal incident idea",
+        "SoloReimu": "putting Solo Reimu in other games for a laugh",
+        "SoloSakuya": "putting Solo Sakuya in other games for a laugh",
+        "TESMgoofs": "TESMgoofs in th15, th15, th06, th07. will be working on other stuff not sure when it'll be out so keep tight",
         "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of"
     }
 }
