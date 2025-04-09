05ff97ef29f5a85e49d1b94aef1b657e7b3233dc
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-04-09 15:00:28 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -11,11 +11,8 @@
         "16Kaizo": "16Kaizo",
         "AltJessPH": "AltJessPH",
         "Data10": "Touhou 10 Kaizo",
-        "Impossible_Character": "Touhou 14 with a few ISC patterns",
-        "KaizoEoSD": "KaizoEoSD",
         "KaizoIN": "Kaizo IN",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "Sleeping": "Sleeping Broken Border. orginal incident idea",
         "SoloSakuya": "putting Solo Sakuya in other games for a laugh",
         "TESMgoofs": "TESMgoofs in th15, th15, th06, th07. will be working on other stuff not sure when it'll be out so keep tight",
         "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of(full patch no game dialouge other than reimu)"
