10de0caf15b855c2be328a50375112ebcca0eca5
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-04-10 21:06:29 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -13,7 +13,7 @@
         "Data10": "Touhou 10 Kaizo",
         "KaizoIN": "Kaizo IN",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "SoloSakuya": "putting Solo Sakuya in other games for a laugh",
+        "SoloSakuya": "putting Solo Sakuya in other games for a laugh for (th07,th10,th11,th12,th13,th14,th15,th16,th17)",
         "TESMgoofs": "TESMgoofs in th15, th06, th07, th11. will be working on other stuff not sure when it'll be out so keep tight",
         "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of(full patch no game dialouge other than reimu)"
     }
