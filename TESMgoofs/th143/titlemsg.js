2aafc53e129996830aad650339f5b2608e6b99ca
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-04-11 07:16:50 +0100

    Add files via upload

---
+++ TESMgoofs/th143/titlemsg.js
@@ -0,0 +1,42 @@
+{
+    "0": [
+        "Hmm simple Patterns I wonder what I will name them,",
+        "Maybe really cool names."
+    ],
+    "1": [
+        "WOW. That was a spike what happened did people want more?",
+        "I guess I got some more names to give out."
+    ],
+    "2": [
+        "Ha...ha...They're Angry right?",
+        "I might need to be more... eye-i? Is that a word?"
+    ],
+    "3": [
+        "How am I not dead from that ghost? ",
+        "I guess...I'm not effected by death :D"
+    ],
+    "4": [
+        "Why does my head start to hurt?",
+        "I need to focus on the names."
+    ],
+    "5": [
+        "What? Did she say again about me?",
+        "Nevermind that. better keep going"
+    ],
+    "6": [
+        "I don't like these people. Marisa and the others seem",
+        "really angry with me!"
+    ],
+    "7": [
+        "W-what?",
+        ""
+    ],
+    "8": [
+        "I need to get away this isn't real",
+        "I'm real!"
+    ],
+    "9": [
+        "Who am I?",
+        "I'm Scared!"
+    ]
+}
