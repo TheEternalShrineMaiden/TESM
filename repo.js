07f118f1152271f6a5581be3a5922de2a39c2249
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-05-05 16:46:33 +0100

    Add files via upload

--- repo.js
+++ repo.js
@@ -6,15 +6,22 @@
         "https://mirrors.thpatch.net/TESM/"
     ],
     "patches": {
-        "13Kaizo": "Touhou 13 Kaizo",	
+        "13Kaizo": "Touhou 13 Kaizo",
         "15Kaizo": "15Kaizo",
         "16Kaizo": "16Kaizo",
         "AltJessPH": "AltJessPH",
         "Data10": "Touhou 10 Kaizo",
+        "Impossible_Character": "Touhou 14 with a few ISC patterns",
+        "KaizoEoSD": "KaizoEoSD",
         "KaizoIN": "Kaizo IN",
         "PC98R": "PC98 Reimu in touhou 18 stage 4 + Flandre on extra stage",
-        "SoloSakuya": "putting Solo Sakuya in other games for a laugh for (th07,th10,th11,th12,th13,th14,th15,th16,th17)",
-        "TESMgoofs": "TESMgoofs in th06(buggy), th07, th11, th14, th14.3. will be working on other stuff not sure when it'll be out so keep tight",
-        "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of(full patch no game dialouge other than reimu)"
+        "Sleeping": "Sleeping Broken Border. orginal incident idea",
+        "SoloReimu": "putting Solo Reimu in other games for a laugh",
+        "SoloSakuya": "putting Solo Sakuya in other games for a laugh",
+        "TESMgoofs": "TESMgoofs in th15, th15, th06, th07. will be working on other stuff not sure when it'll be out so keep tight",
+        "THMiDI": "Touhou Minds in Dreaming Individuals, a fangame fanpatch idea that I thought of",
+        "WBaLK": "TH17 with TH15 stuff the best i can do lmao",
+	    "FWtrBR": "Boss rush for Touhou 20's Demo",
+        "betterHitbox": "fixing transformation bullets example kanako's spells"
     }
 }
