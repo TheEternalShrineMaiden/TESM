d09e51347a162b969943950cf4d7ef8a34b4a050
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-11-21 13:11:27 +0000

    Add files via upload

--- TESMgoofs/patch.js
+++ TESMgoofs/patch.js
@@ -1,17 +1,11 @@
 {
-  "dependencies": [
-    "SuperChrim/Th19StoryBGMChange",
-    "ExpHP/base_exphp",
-	"ZeroEclExtensions",
-    "ExpHP/anm_leak",
-    "ExpHP/continue",
-    "ExpHP/sprite_death_fix",
-    "ExpHP/bullet_cap",
-    "thpatch/lang_en"
-  ],
-  "id": "TESMgoofs",
-  "servers": [
-    "https://mirrors.thpatch.net/TESM/TESMgoofs/"
-  ],
-  "title": "TESMgoofs are like Kaizo but alittle more thoughts in there in th06, th07, th11, th14, th14.3 and th16. will be working on other stuff not sure when it'll be out so keep tight"
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "ZeroEclExtensions",
+	"servers": [
+		"https://raw.githubusercontent.com/zero318/thcrap_patches/master/patches/ZeroEclExtensions/"
+	],
+	"title": "(6-9,14,14.3,18,19,20) Adds new ECL instructions",
+	"version": "0.0.0.4"
 }
