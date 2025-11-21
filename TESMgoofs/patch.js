c7f9629426c98e73b126c8acdbab02bf050dd00e
TESM <114009303+TheEternalShrineMaiden@users.noreply.github.com>
2025-11-21 13:08:07 +0000

    Add files via upload

--- TESMgoofs/patch.js
+++ TESMgoofs/patch.js
@@ -1,16 +1,17 @@
 {
-	"dependencies": [
-		"SuperChrim/Th19StoryBGMChange",
-		"ExpHP/base_exphp",
-		"ExpHP/anm_leak",
-		"ExpHP/continue",
-		"ExpHP/sprite_death_fix",
-		"ExpHP/bullet_cap",
-		"thpatch/lang_en"
-	],
-	"id": "TESMgoofs",
-	"servers": [
-		"https://mirrors.thpatch.net/TESM/TESMgoofs/"
-	],
-	"title": "TESMgoofs are like Kaizo but alittle more thoughts in there in th06, th07, th11, th14, th14.3 and th16. will be working on other stuff not sure when it'll be out so keep tight"
+  "dependencies": [
+    "SuperChrim/Th19StoryBGMChange",
+    "ExpHP/base_exphp",
+	"ZeroEclExtensions",
+    "ExpHP/anm_leak",
+    "ExpHP/continue",
+    "ExpHP/sprite_death_fix",
+    "ExpHP/bullet_cap",
+    "thpatch/lang_en"
+  ],
+  "id": "TESMgoofs",
+  "servers": [
+    "https://mirrors.thpatch.net/TESM/TESMgoofs/"
+  ],
+  "title": "TESMgoofs are like Kaizo but alittle more thoughts in there in th06, th07, th11, th14, th14.3 and th16. will be working on other stuff not sure when it'll be out so keep tight"
 }
